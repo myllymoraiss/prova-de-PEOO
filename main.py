1a80d272b5eef8c8bae110d859259d50e1495397
myllymoraiss <kemylly.morais@escolar.ifrn.edu.br>
2024-07-22 17:15:32 -0300

    adicionei main

---
+++ main.py
@@ -0,0 +1,6 @@
+import math
+
+x = int(input("Coloque o primeiro ponto do plano: "))
+y = int(input("Coloque o segundo ponto do plano: "))
+
+print(f"O resultado é: {math.sqrt(((x*2-x)**2+(y*2-y)**2))}")
